694df80fc7c8778f6982d76244e62e2a0b172571
MathurManish <manishmannmathur@gmail.com>
2020-07-30 13:34:07 +0530

    Picture Added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Welcom to Jodhpur'
     };
   }
 
@@ -18,6 +18,7 @@ class App extends Component {
         <p>
           Start editing to see some magic happen :)
         </p>
+        <img width="100%" src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" />
       </div>
     );
   }
